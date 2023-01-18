a421f008c9494d72ff6156082a9b3ff7e2127055
Angelica dos Santos <santosangelicassp@gmail.com>
2023-01-18 13:20:38 -0300

    Initial Commit

---
+++ Drawer/DrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerApp.swift
+//  Drawer
+//
+//  Created by Angelica dos Santos on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
